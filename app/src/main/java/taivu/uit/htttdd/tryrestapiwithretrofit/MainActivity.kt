e5ab0c1199c5d7d2154c767346469edb00c0c22a
vcttai <vcttai@gmail.com>
2021-07-23 23:03:19 +0700

    remove files for another example

--- app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/MainActivity.kt
+++ app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/MainActivity.kt
@@ -9,25 +9,3 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
-
-
-//package taivu.uit.htttdd.tryrestapiwithretrofit
-//import android.os.Bundle
-//import android.view.View
-//import android.widget.TextView
-//import androidx.appcompat.app.AppCompatActivity
-//
-//
-//class MainActivity : AppCompatActivity() {
-//    var responseText: TextView? = null
-//    var apiInterface: APIInterface? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-////        responseText = findViewById<View>(R.id.responseText) as TextView
-//
-//        apiInterface = APIClient.getClient().create<APIInterface>(APIInterface::class.java)
-//    }
-//}
