2e099c603f31523c5917ed163eeadcfdb6589043
vcttai <vcttai@gmail.com>
2021-07-24 15:22:25 +0700

    Fetch data in ViewModel

--- app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/MainActivity.kt
+++ app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/MainActivity.kt
@@ -2,23 +2,43 @@ package taivu.uit.htttdd.tryrestapiwithretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
-import taivu.uit.htttdd.tryrestapiwithretrofit.service.movie.MovieFetcherByCoroutine
+import androidx.lifecycle.ViewModelProvider
+import taivu.uit.htttdd.tryrestapiwithretrofit.service.movie.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var movieVM: MovieViewModel;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.movieVM = ViewModelProvider(this).get(MovieViewModel::class.java)
     }
 
     fun handleClickCallAPI(view: View) {
         val txtResultView = findViewById<TextView>(R.id.result_container);
-        val movieFetcher = MovieFetcherByCoroutine();
 
         txtResultView.text = getString(R.string.status_fetching);
-        movieFetcher.getNowPlayingMovies();
+
+        this.movieVM.getNowPlayingMovies();
 
         txtResultView.text = getString(R.string.status_fetched);
+    }
+
+    fun handleClickTest(view: View) {
+        val txtResultView = findViewById<TextView>(R.id.result_container);
+
+        if(this.movieVM.nowPlayingMovies == null) {
+            txtResultView.text = "Need to fetch data first!";
+            return;
+        }
+
+        val movie = this.movieVM.nowPlayingMovies?.results?.get(0) ?: return;
+        txtResultView.text = movie.title.toString();
+
+        Log.e("[Main Activity]", movie.title.toString());
     }
 }
--- app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/service/movie/MovieViewModel.kt
+++ app/src/main/java/taivu/uit/htttdd/tryrestapiwithretrofit/service/movie/MovieViewModel.kt
@@ -4,10 +4,21 @@ import android.util.Log
 import kotlinx.coroutines.launch
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import taivu.uit.htttdd.tryrestapiwithretrofit.model.MovieResp
 import taivu.uit.htttdd.tryrestapiwithretrofit.requestbuilder.RequestBuilder
 
 
-class MovieFetcherByCoroutine : ViewModel() {
+class MovieViewModel : ViewModel() {
+    companion object {
+        private val TAG = MovieViewModel::class.java.simpleName
+    }
+
+    var nowPlayingMovies: MovieResp? = null
+
+    init {
+        Log.e("[${TAG}]", "Init view model")
+    }
+
     fun getNowPlayingMovies() {
         viewModelScope.launch {
             val requestBuilder = RequestBuilder(
@@ -17,12 +28,18 @@ class MovieFetcherByCoroutine : ViewModel() {
             val movieService = requestBuilder.createRequestBuilderForApi()
                 .create(MovieDBService::class.java);
 
-            val nowPlayingMovies = movieService.listNowPlayingMovies(
+            nowPlayingMovies = movieService.listNowPlayingMovies(
                 language = "en-US",
                 page = 1
             )
 
-            Log.e("[Now playing]", nowPlayingMovies.results?.get(0).toString());
-        }
+            Log.e("[Now playing]", nowPlayingMovies?.results?.get(0).toString());
+        };
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        Log.e("[${TAG}]", "Clear View Model");
     }
 }
